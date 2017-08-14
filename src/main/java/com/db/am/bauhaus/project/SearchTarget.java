cb680a9e59ca5e5d9ad914496e0f7869932dd4a0
chandank <chandan.kumar@thetrainline.com>
2017-08-14 10:25:32 +0100

    wip test implemented

--- src/main/java/com/db/am/bauhaus/project/SearchTarget.java
+++ src/main/java/com/db/am/bauhaus/project/SearchTarget.java
@@ -7,8 +7,14 @@ import org.openqa.selenium.By;
  * Created by ongshir on 06/04/2017.
  */
 public class SearchTarget {
+    //Updated few locators as they were no longer valid on web page
+
     public final static Target INPUT_BOX = Target.the("the search input box").located(By.id("search-query"));
-    public final static Target INPUT_BOX_BUTTON = Target.the("the search input button").located(By.cssSelector(".btn.btn-orange.btn-append"));
-    public final static Target TOP_CATEGORIES_HEADER = Target.the("the top categories header").located(By.cssSelector("h4.pb-xs-1-5"));
-    public final static Target ALL_CATEGORIES_HEADER = Target.the("the all categories header").located(By.cssSelector("h1.conform-heading.display-inline"));
+    //public final static Target INPUT_BOX_BUTTON = Target.the("the search input button").located(By.cssSelector(".btn.btn-orange.btn-append"));
+    public final static Target INPUT_BOX_BUTTON = Target.the("the search input button").located(By.cssSelector("button.btn.btn-primary"));
+    //public final static Target TOP_CATEGORIES_HEADER = Target.the("the top categories header").located(By.cssSelector("h4.pb-xs-1-5"));
+    public final static Target TOP_CATEGORIES_HEADER = Target.the("the top categories header").located(By.cssSelector(".pb-xs-1-5"));
+    //public final static Target ALL_CATEGORIES_HEADER = Target.the("the all categories header").located(By.cssSelector("h1.conform-heading.display-inline"));
+    public final static Target ALL_CATEGORIES_HEADER = Target.the("the all categories header").located(By.cssSelector("h1.display-inline"));
+    public final static Target SORT_BY = Target.the("the sorting ").located(By.cssSelector("div#sortby"));
 }
--- src/test/java/com/db/am/bauhaus/project/steps/SearchSteps.java
+++ src/test/java/com/db/am/bauhaus/project/steps/SearchSteps.java
@@ -1,11 +1,10 @@
 package com.db.am.bauhaus.project.steps;
 
-import com.db.am.bauhaus.project.SearchFor;
-import com.db.am.bauhaus.project.SearchTarget;
-import com.db.am.bauhaus.project.SessionVar;
+import com.db.am.bauhaus.project.*;
 import com.db.am.bauhaus.project.pages.MainSearchPage;
 import com.db.am.bauhaus.project.steplib.SearchUser;
 import cucumber.api.java.Before;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -19,6 +18,7 @@ import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
 import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
 import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.containsText;
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
 import static net.serenitybdd.screenplay.questions.WebElementQuestion.the;
 
 /**
@@ -68,6 +68,39 @@ public class SearchSteps {
         theActorInTheSpotlight().should(
                 seeThat("the top categories header ", the(SearchTarget.TOP_CATEGORIES_HEADER), containsText(searchText)),
                 seeThat("the all categories header ", the(SearchTarget.ALL_CATEGORIES_HEADER), containsText(searchText))
+        );
+    }
+
+    @When("^s?he selects (.*) under navigation menu (.*)$")
+    public void heSelectsPoetryUnderNavigationMenuEntertainmentScreenplay(String selectText, String navigationMenu) throws Throwable {
+        theActorInTheSpotlight().attemptsTo(
+                SelectFromDropDownMenu
+                        .textCalled(navigationMenu, selectText)
+        );
+    }
+
+    @Then("^the result should be displayed for selected option$")
+    public void theResultShouldBeDisplayedForSelectedOptionScreenplay() throws Throwable {
+        String selectedText = Serenity.sessionVariableCalled(SessionVar.SELECT_TEXT).toString();
+        theActorInTheSpotlight().should(
+                seeThat("the menu path header ", the(SelectFromDropDownTarget.MENU_PATH_HEADER), containsText(selectedText)),
+                seeThat("the page header ", the(SelectFromDropDownTarget.PAGE_HEADER), containsText(selectedText)),
+                seeThat("the all categories header ", the(SelectFromDropDownTarget.ALL_CATEGORIES_HEADER), containsText(selectedText))
+        );
+    }
+
+    @When("^s?he selects (.*) icon$")
+    public void heSelectsClothingIcon(String selectIconText) throws Throwable {
+        theActorInTheSpotlight().attemptsTo(
+                SelectFromIcons
+                        .textCalled(selectIconText)
+        );
+    }
+
+    @And("^he should see the sorting option$")
+    public void heShouldSeeTheSortingOption() throws Throwable {
+        theActorInTheSpotlight().should(
+                seeThat("the sort by section", the(SearchTarget.SORT_BY), isVisible())
         );
     }
 }
